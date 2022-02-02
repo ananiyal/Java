1b718a1ca4ccbb912a2c43bc24b1746f186c9427
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-02 16:27:27 +0530

    Add files via upload

---
+++ Practical/arrayexample3.java
@@ -0,0 +1,11 @@
+package Ananiyal;
+
+public class arrayexample3 {
+		
+	public static void main(String[] args){
+		int a[]={10,20,30,40,50};//directly and initializing and instantiating
+		for(int i:a)// for each loop prints the array one by one
+			System.out.println(i);
+	}
+
+}
