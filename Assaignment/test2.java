7699bf5a85ef3953b5c09bb43f56063444eb5656
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-04 00:29:20 +0530

    Add files via upload

---
+++ Assaignment/test2.java
@@ -0,0 +1,10 @@
+package ananiyal;
+
+public class test2 {
+	public static void main(String[] args)
+	{
+   	 String str[] = { "First", "Second", "Third" };
+   	 for (int i = 0; i < str.length; i++)
+        System.out.print(str[i]);
+	}
+}
