3aa8b8982fab42be7479bac76ea20fb465b54a08
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-04 00:22:34 +0530

    Add files via upload

---
+++ Assaignment/driver5.java
@@ -0,0 +1,16 @@
+package ananiyal;
+
+public class driver5 {
+	public static void main ( String [] args ) 
+	{ 
+	int [] intArr = new int [5]; 
+	for (int i = 0; i < intArr . length ; i ++) 
+	{
+	 intArr [ i ] = i * 2;
+	 }
+	 for (int i = 0; i < intArr . length ; i ++) 
+	{ 
+	System . out . print ( intArr [ i ] + ", ") ;
+	 }
+	 } 
+}
