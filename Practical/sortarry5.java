c934b46a05264c678c117e17cbb9ed93f0c32f97
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-05 15:17:11 +0530

    Add files via upload

---
+++ Practical/sortarry5.java
@@ -0,0 +1,14 @@
+package ananiyal;
+import java.util.Arrays;
+
+public class sortarry5 {
+
+	public static void main(String[] args) {
+		String [] array = {"Mango","Apple","Grapes","Papaya","Pineapple","Bannana","Orange"};   
+		   
+		Arrays.sort(array);  
+		System.out.println("Array elements in descending order: " +Arrays.toString(array));
+
+	}
+
+}
