6960718519e581185e36c9f41f8e5e14cb26cca8
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-11 15:53:39 +0530

    Add files via upload

---
+++ Practical/Array/SwapByValue.java
@@ -0,0 +1,22 @@
+package ananiyal;
+
+public class SwapByValue {
+
+	public static void main(String[] args) {
+		int a = 5000;
+	      int b = 3000;
+	      System.out.println("Before swapping, a = " + a + " and b = " + b);
+	      swapFunction(a, b);
+	      System.out.println("\n*Now, Before and After swapping values will be same here*:");
+	      System.out.println("After swapping, a = " + a + " and b is " + b);
+	   }
+	   public static void swapFunction(int a, int b) {
+	      System.out.println("Before swapping(Inside), a = " + a + " b = " + b);
+	      int c = a;
+	      a = b;
+	      b = c;
+	      System.out.println("After swapping(Inside), a = " + a + " b = " + b);
+	   }
+
+	}
+
