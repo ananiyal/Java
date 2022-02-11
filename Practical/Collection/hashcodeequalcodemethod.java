5208b21abb47e0c9243d00d03823196d40c5305e
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-11 14:23:51 +0530

    Add files via upload

---
+++ Practical/Collection/hashcodeequalcodemethod.java
@@ -0,0 +1,27 @@
+package ananiyal;
+import java.io.*;
+
+class hashcodeequalcodemethod  {
+	public String name;
+    public int id;
+	public hashcodeequalcodemethod(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+		return this.id;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if(obj == null || obj.getClass()!= this.getClass())
+            return false;
+		hashcodeequalcodemethod e = (hashcodeequalcodemethod) obj;
+		return(e.name == this.name && e.id == this.id);
+	}
+	
+
+}
