908e0d4081aad2655562ff1146fc49840fa4f80c
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-14 17:02:50 +0530

    Add files via upload

---
+++ Project/myname/src/main/java/com/ananiyal/myname/App.java
@@ -0,0 +1,9 @@
+package com.ananiyal.myname;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World , My name is Ananiyal" );
+    }
+}
