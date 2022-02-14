39957e6fddc94a06960783bc0ac8c0d8af605f4d
Ananiyal Tudu <98313994+ananiyal@users.noreply.github.com>
2022-02-14 17:00:02 +0530

    Add files via upload

---
+++ Project/HelloMaven/src/main/java/com/ananiyal/HelloMaven/App.java
@@ -0,0 +1,13 @@
+package com.ananiyal.HelloMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
